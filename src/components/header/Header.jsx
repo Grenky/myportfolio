dab712ff43eef601fcace17ddbfcf9a53919b7e6
Grenky <demosoft62@gmail.com>
2025-06-30 21:50:08 +0300

    2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import './styles/headerStyle.css';
-
+import linkedin from '../../assets/images/linkedin-logo.png';
+import telegram from '../../assets/images/telegram.png';
+import gmail from '../../assets/images/gmail.png';
 
 export default function Header() {
     return(
@@ -8,15 +10,17 @@ export default function Header() {
                 <span className='logo-icon'>{"< >"}</span>
                 <span className='logo-text'>Dmitro.Dev</span>
             </div>
-            <nav>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+            <nav className='nav'>
+                <ul className='nav-container'>
+                    <li><a href='/'>About Me</a></li>
+                    <li><a href='/'>Resume</a></li>
+                    <li><a href='/'>Contacts</a></li>
                 </ul>
             </nav>
             <div className='social-box'>
-
+                <img className='linkedin' src={linkedin} alt='linkedin'/>
+                <img className='telegram' src={telegram} alt='telegram'/>
+                <img className='gmail' src={gmail} alt='gmail'/>
             </div>
         </div>
     )

6b317cfc29f841ae584bbb07a5cbdde15aa197f8
Grenky <demosoft62@gmail.com>
2025-06-30 17:16:32 +0300

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import './styles/headerStyle.css';
+
+
+export default function Header() {
+    return(
+        <div className="header-wrapper">
+            <div className='logo-wrapper'>
+                <span className='logo-icon'>{"< >"}</span>
+                <span className='logo-text'>Dmitro.Dev</span>
+            </div>
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+            <div className='social-box'>
+
+            </div>
+        </div>
+    )
+}
---
+++ src/homepage/HomePage.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/header/Header"
+
+
+export default function HomePage() {
+    return(
+        <div className="home-wrapper">
+            <Header />
+            <h1>TEST</h1>
+        </div>
+    )
+}

a4663abf85d3ca682135fdcd6e293b2767f8b072
Grenky <demosoft62@gmail.com>
2025-07-03 20:42:21 +0300

    4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import './styles/global.scss'
 import HomePage from './pages/Home/HomePage';
 
 function App() {
--- src/pages/Home/HomeComponents/HomeContent/HomeContent.jsx
+++ src/pages/Home/HomeComponents/HomeContent/HomeContent.jsx
@@ -1,20 +1,50 @@
 import HomeConStyles from './HomeContent.module.scss';
 import MyImg from '../../../../assets/images/image.png';
+import { useEffect, useState } from 'react';
+
+
+
 
 export default function HomeContent() {
+
+const fullText = `<span class="tag">&lt;p&gt;</span> On my way to improve myself in advanced technologies such as <span class="tag">ReactJS, NodeJS, and TypeScript.</span> My knowledge will help you create new web products that will help people realize their plans and dreams.<span class="tag"> &lt;/p&gt;</span>`;
+const [displayedText, setDisplayedText] = useState('');
+const [showeCursor, setShowCursor] = useState(true);
+
+    useEffect(() => {
+        let i = 0;
+        const interval = setInterval(() => {
+            if(i < fullText.length) {
+                setDisplayedText(fullText.slice(0, i + 1));
+                i++;
+            } else {
+                clearInterval(interval)
+                setTimeout(() => setShowCursor(false), 1000);
+            }        
+        }, 15);
+        return () => clearInterval(interval); 
+    }, [fullText]);
+
+
     return(
-        <div className={HomeConStyles.wrapper}>
+        <section  className={HomeConStyles.wrapper}>
             <div className={HomeConStyles['img-box']}>
                 <img className={HomeConStyles.myImg} src={MyImg} alt={MyImg}></img>
             </div>
-            <div className={HomeConStyles.text}>
-                <p className={HomeConStyles['first-text']}>&lt;span&gt; Hi, I'm Dmitro &lt;/span&gt;</p>
-                <h1>Junior <span>{"{ FRONTEND }"}</span>Web Developer</h1>
-                <p>
-                    &lt;p&gt;On my way to improve myself in advanced technologies such as ReactJS, NodeJS, and TypeScript. 
-                    My knowledge will help you create new web products that will help people realize their plans and dreams.&lt;/p&gt;
-                </p>              
-            </div>
-        </div>
+            <article className={HomeConStyles.text}>
+                <p className={HomeConStyles['first-text']}>
+                    <span className={HomeConStyles.tag}>&lt;span&gt; </span>
+                        Hi, I'm Dmitro
+                    <span className={HomeConStyles.tag}>  &lt;span&gt;</span> 
+                </p>
+                <h1 className={HomeConStyles.title}>Junior <span className={HomeConStyles['title-tag']}>{"{ Frontend }"}</span> Web Developer</h1>
+                <p className={HomeConStyles['last-text']}>
+                    <span  dangerouslySetInnerHTML={{ __html: displayedText }} />
+                    {showeCursor && <span className={HomeConStyles.cursor}></span>}
+                </p>
+                
+                                 
+            </article>
+        </section >
     )
 }

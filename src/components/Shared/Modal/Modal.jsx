6b194f621d72b3c591aba6c961327ac5565aabb9
Grenky <demosoft62@gmail.com>
2025-07-11 14:11:03 +0300

    9

---
+++ src/components/Shared/Modal/Modal.jsx
@@ -0,0 +1,10 @@
+import ModalStyles from './styles/ModalStyles.module.scss';
+
+
+export default function Modal() {
+    return(
+        <div className={ModalStyles['modal-wrapper']}>
+
+        </div>
+    )
+}
